46a01ad3176ff56a4bcaaa017a98111952c4fa18
codeDealer <bacar.darwin.github@hotmail.com>
2021-12-08 16:12:59 +0100

    first commit

---
+++ scorpion_json_cli/LISEZMOI.md
@@ -0,0 +1,4 @@
+# EXERCICE PYTHON
+Création d'un exercice en python pour effectuer des opérations CRUD en python.
+## Lien de la source
+[lien source] (https://dhruvpadhiyar.medium.com/basic-crud-operations-with-json-8fa879204031)
---
+++ scorpion_json_cli/scorpion_cli.py
@@ -0,0 +1,34 @@
+import json
+
+# class CRUD json
+class Crud_Json:
+    
+    def __init__(self):
+        pass
+
+    def chargement_data(self, data_json):
+        with open(data_json) as fichier_json:
+            return json.load(fichier_json)
+    
+    def recuperer_un_item_dans_json(self, json, id):
+        for element in json:
+            ''
+        
+
+# fonction princpale
+def main():
+    crud_json = Crud_Json()
+    input_name_json = input('entrez nom du fichier json : ')
+    input_id = input('entrez id (convertit en string) : ')
+    
+    json_file_recuperer = crud_json.chargement_data(input_name_json)
+    print(json_file_recuperer['date_created'])
+    print(type(input_id))
+    
+    crud_json.recuperer_un_item_dans_json(json_file_recuperer, input_id)
+    # print(element_recuperer)
+    
+    
+    
+if __name__ == "__main__":
+    main()
